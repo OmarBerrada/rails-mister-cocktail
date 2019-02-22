0e2195922402a24391b9245671a0a1c55d4a60c7
Omar Berrada <berradaomar@yahoo.com>
2019-02-22 17:04:51 +0100

    App's features deployed and operational

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,9 @@
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Tomato")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "salt")
+Ingredient.create(name: "eggs")
+Ingredient.create(name: "fish")
+
